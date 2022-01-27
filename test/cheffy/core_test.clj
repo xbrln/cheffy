3419819609f18d09e133a9c9d070e610a74633c4
jef <324976+xbrln@users.noreply.github.com>
2022-01-27 16:24:21 +0100

    Add integrant, swagger and first route

--- test/cheffy/core_test.clj
+++ test/cheffy/core_test.clj
@@ -1,6 +1,5 @@
 (ns cheffy.core-test
-  (:require [clojure.test :refer :all]
-            [cheffy.core :refer :all]))
+  (:require [clojure.test :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
